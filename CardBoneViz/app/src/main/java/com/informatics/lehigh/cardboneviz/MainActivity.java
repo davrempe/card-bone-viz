1ee59f766cb879a66c1a20244c073f2df7eb31f3
Davis Rempe <davrempe@gmail.com>
2016-06-10 14:50:47 -0400

    Added new cardboard AR library for stereo rendering etc..

--- CardBoneViz/app/src/main/java/com/informatics/lehigh/cardboneviz/MainActivity.java
+++ CardBoneViz/app/src/main/java/com/informatics/lehigh/cardboneviz/MainActivity.java
@@ -1,13 +1,50 @@
 package com.informatics.lehigh.cardboneviz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.vr.sdk.base.Eye;
+import com.google.vr.sdk.base.GvrActivity;
+import com.google.vr.sdk.base.GvrView;
+import com.google.vr.sdk.base.HeadTransform;
+import com.google.vr.sdk.base.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class MainActivity extends GvrActivity implements GvrView.StereoRenderer {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
     }
 }
---
+++ CardBoneViz/cardboardarlibrary/src/main/java/com/informatics/lehigh/cardboardarlibrary/StereoCamera.java
@@ -0,0 +1,8 @@
+package com.informatics.lehigh.cardboardarlibrary;
+
+/**
+ * Created by Davis on 6/10/2016.
+ */
+public class StereoCamera {
+
+}
